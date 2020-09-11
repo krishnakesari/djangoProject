6a82945e38d47a500759af70a92073098bf299e7
Krishna <bkesari@outlook.com>
2020-09-11 16:59:51 -0400

    Added order placed message

--- pizza/views.py
+++ pizza/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .forms import PizzaForm
 # Create your views here.
 # 5. Add request function for home page
 def home (request):
@@ -7,4 +7,15 @@ def home (request):
 
 # 6. Add request function for order page
 def order (request):
-    return render(request,'pizza/order.html')
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST)
+        if filled_form.is_valid():
+            note = 'Thanks for ordering! Your %s %s and %s pizza is on its way !' %(filled_form.cleaned_data['size'],
+                                                                                    filled_form.cleaned_data['topping1'],
+                                                                                    filled_form.cleaned_data['topping2'],)
+            new_form = PizzaForm()
+            return render(request, 'pizza/order.html', {'pizzaform': new_form, 'note': note})
+    else:
+        form = PizzaForm()
+        return render(request,'pizza/order.html', {'pizzaform': form})
+
