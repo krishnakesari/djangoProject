8ffac07e24d81e02522b9a69d05be2eed3d378b0
Krishna <bkesari@outlook.com>
2020-09-11 16:31:05 -0400

    Basic order form

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pizza import views # 1. Importing Views from pizza
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'), # 2. Adding home page
+    path('order', views.order, name='order'), # 3. Adding a tab / link
 ]
--- pizza/views.py
+++ pizza/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# 5. Add request function for home page
+def home (request):
+    return render(request,'pizza/home.html')
+
+# 6. Add request function for order page
+def order (request):
+    return render(request,'pizza/order.html')
